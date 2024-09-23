d9f7c027cc8335650f8e7031837a490334519dda
JuliaTBarros <162466823+JuliaTBarros@users.noreply.github.com>
2024-09-22 21:41:56 -0300

    correção de responsividade, css num geral, criação de dropdown checkboxes

--- which_teacher/which_teacher_app/templates/teste.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const dropdownBtn = document.querySelector(".dropdown-btn");
-    const dropdownCheckbox = document.querySelector(".dropdown-checkbox");
-
-    dropdownBtn.addEventListener("click", function () {
-        dropdownCheckbox.classList.toggle("show-dropdown");
-    });
-});
--- which_teacher/which_teacher_app/templates/which_teacher_app/js/cadastroProfessor.js
+++ which_teacher/which_teacher_app/templates/which_teacher_app/js/cadastroProfessor.js
@@ -6,3 +6,25 @@ document.addEventListener("DOMContentLoaded", function () {
         dropdownCheckbox.classList.toggle("show-dropdown");
     });
 });
+
+document.querySelectorAll('.dropdown-btn').forEach(button => {
+    button.addEventListener('click', event => {
+        event.preventDefault();
+    });
+});
+
+document.querySelectorAll('.dropdown-btn').forEach(btn => {
+    btn.addEventListener('click', event => {
+        event.stopPropagation();
+        btn.parentNode.classList.toggle('show-dropdown');
+    });
+});
+
+const dropdownBtns = document.querySelectorAll('.dropdown-btn');
+const dropdownLists = document.querySelectorAll('.dropdown-list');
+
+dropdownBtns.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        dropdownLists[index].classList.toggle('show');
+    });
+});
