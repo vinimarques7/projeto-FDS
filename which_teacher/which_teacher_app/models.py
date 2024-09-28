5e76d969aaea5dfb5a38f5b46d99d4e0a26bbc2d
Matheus Verissimo <matheusmverissimo820@gmail.com>
2024-09-27 23:15:47 -0300

    Adicionar modelo de professor e atualizar formulário de login
    Adicionado um novo modelo chamado professor com campos para id, email e senha. Também atualizou o modelo de formulário de login para incluir os atributos de nome para entradas de e-mail e senha.

--- which_teacher/which_teacher_app/models.py
+++ which_teacher/which_teacher_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class professor(models.Model):
+    id_prof = models.AutoField(primary_key=True)
+    email = models.EmailField(max_length=100)
+    senha = models.CharField(max_length=100)
--- which_teacher/which_teacher_app/views.py
+++ which_teacher/which_teacher_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import professor
 
 def home(request):
     return render(request, 'which_teacher_app/landingPage.html')
@@ -11,3 +12,9 @@ def cadastroP(request):
 
 def perfilP(request):
     return render(request, 'which_teacher_app/perfilProfessor.html')
+
+def UsuProf(request):
+    novo_professor = professor()
+    novo_professor.email = request.POST.get('email')
+    novo_professor.senha = request.POST.get('senha')
+    novo_professor.save()
