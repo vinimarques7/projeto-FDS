30d768d0c11eae8447646d4e06533449d7c4b68e
Matheus Verissimo <matheusmverissimo820@gmail.com>
2024-09-29 00:53:25 -0300

    alterei a view de cadastro para remover o confirmar senha

--- which_teacher/which_teacher_app/views.py
+++ which_teacher/which_teacher_app/views.py
@@ -9,29 +9,25 @@ def cadastro_professor(request):
         celular = request.POST.get('celular')
         email = request.POST.get('email')
         senha = request.POST.get('senha')
-        confirmar_senha = request.POST.get('confirmar_senha')
         materia = request.POST.getlist('materia')
         nivel_ensino = request.POST.getlist('nivel_ensino')
         recebimento = request.POST.getlist('recebimento')
         comunicacao = request.POST.getlist('comunicacao')
         genero = request.POST.get('genero')
 
-        if senha != confirmar_senha:
-            return render(request, 'which_teacher_app/cadastroProfessor.html', {'error': 'Senhas não conferem'})
-        else:
-            professor = Professor(
-                nome=nome,
-                celular=celular,
-                email=email,
-                senha=senha,
-                materia=', '.join(materia),
-                nivel_ensino=', '.join(nivel_ensino),
-                recebimento=', '.join(recebimento),
-                comunicacao=', '.join(comunicacao),
-                genero=genero
-            )
-            professor.save()
-            return redirect('which_teacher_app/loginProfessor.html')
+        professor = Professor(
+            nome=nome,
+            celular=celular,
+            email=email,
+            senha=senha,
+            materia=', '.join(materia),
+            nivel_ensino=', '.join(nivel_ensino),
+            recebimento=', '.join(recebimento),
+            comunicacao=', '.join(comunicacao),
+            genero=genero
+        )
+        professor.save()
+        return redirect('which_teacher_app/loginProfessor.html')
     else:
         return render(request, 'which_teacher_app/cadastroProfessor.html')
     
