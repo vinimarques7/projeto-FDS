27ccc9861a0201cb26649ad69cb98086ef28fdfe
Vinícius Bernardo <vbs4@cesar.school>
2024-09-29 02:47:43 -0300

    consertei o dropdown de materias

--- which_teacher/which_teacher_app/static/js/cadastroProfessor.js
+++ which_teacher/which_teacher_app/static/js/cadastroProfessor.js
@@ -1,30 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const dropdownBtn = document.querySelector(".dropdown-btn");
-    const dropdownCheckbox = document.querySelector(".dropdown-checkbox");
-
-    dropdownBtn.addEventListener("click", function () {
-        dropdownCheckbox.classList.toggle("show-dropdown");
-    });
-});
-
-document.querySelectorAll('.dropdown-btn').forEach(button => {
-    button.addEventListener('click', event => {
-        event.preventDefault();
-    });
-});
-
 document.querySelectorAll('.dropdown-btn').forEach(btn => {
     btn.addEventListener('click', event => {
-        event.stopPropagation();
-        btn.parentNode.classList.toggle('show-dropdown');
+      event.preventDefault();
+      event.stopPropagation();
+      btn.parentNode.classList.toggle('show-dropdown');
     });
-});
-
-const dropdownBtns = document.querySelectorAll('.dropdown-btn');
-const dropdownLists = document.querySelectorAll('.dropdown-list');
-
-dropdownBtns.forEach((btn, index) => {
-    btn.addEventListener('click', () => {
-        dropdownLists[index].classList.toggle('show');
-    });
-});
+  });
