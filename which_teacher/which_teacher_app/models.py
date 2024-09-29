1e9d48a9409a6b592e81329dadb8ebbfb98f713b
Matheus Verissimo <matheusmverissimo820@gmail.com>
2024-09-28 22:14:43 -0300

    retirei a a row de confimação de senha do banco de dados

--- which_teacher/which_teacher_app/models.py
+++ which_teacher/which_teacher_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Professor(models.Model):
     email = models.EmailField(unique=True)
     senha = models.CharField(max_length=255)
-    confirma_senha = models.CharField(max_length=255)
+    
 
     def __str__(self):
         return self.email
