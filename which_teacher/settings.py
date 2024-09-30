e27c3c6df160522bf48f6996e43f112e7cfe69ac
Matheus M <168973700+MatheusMV05@users.noreply.github.com>
2024-09-30 12:16:12 -0300

    Update settings.py

--- which_teacher/settings.py
+++ which_teacher/settings.py
@@ -26,16 +26,22 @@ TARGET_ENV = os.getenv('TARGET_ENV')
 NOT_PROD = not TARGET_ENV.lower().startswith('prod')
 
 if NOT_PROD:
-    # Ambiente de desenvolvimento
+    # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = True
-    SECRET_KEY = 'django-insecure-&jj*w)_d4w8o18*llk(!gnw86ys#j3hmwttjz#e7d+(*(9=+aj'  # Substitua com sua SECRET_KEY original
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = 'django-insecure-&jj*w)_d4w8o18*llk(!gnw86ys#j3hmwttjz#e7d+(*(9=+aj'
     ALLOWED_HOSTS = []
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 else:
-    # Ambiente de produção
     SECRET_KEY = os.getenv('SECRET_KEY')
     DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
     ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
-    ALLOWED_HOSTS.append('whichteacher-c0ceg4bueke5cec0.eastus-01.azurewebsites.net')
+    ALLOWED_HOSTS.append( 'whichteacher-c0ceg4bueke5cec0.eastus-01.azurewebsites.net')
     CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split(' ')
     SECURE_SSL_REDIRECT = os.getenv('SECURE_SSL_REDIRECT', '0').lower() in ['true', 't', '1']
 
