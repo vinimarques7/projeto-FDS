02a37ddc4aa6b185ecd8912050ba7ab1eeeb6366
Matheus Verissimo <matheusmverissimo820@gmail.com>
2024-10-21 16:39:43 -0300

    Refactor cadastroProfessor.js and urls.py, and update templates and static files

--- which_teacher_app/urls.py
+++ which_teacher_app/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('loginP/', views.loginP, name='loginP'),
     path('cadastroP/', views.cadastro_professor, name='cadastroP'),
     path('perfilP/', views.perfilP, name='perfilP'),
+    path('cadastroA/', views.cadastro_aluno, name='cadastroP'),
+    path('loginA/', views.loginA, name='loginP'),
     path('busca/', views.busca, name='busca'),
 ]
--- which_teacher_app/views.py
+++ which_teacher_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Professor, Horario
+from .models import Professor, Horario,Aluno
 from django.contrib import messages
 
 def cadastro_professor(request):
@@ -65,11 +65,51 @@ def perfilP(request):
         
     return render(request, 'perfilProfessor.html', {'professor': professor, 'horarios': horarios})
 
+def cadastro_aluno(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        celular = request.POST.get('celular')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        materia = request.POST.getlist('materia')
+        nivel_ensino = request.POST.getlist('nivel_ensino')
+        idade = request.POST.get('idade')
+        genero = request.POST.get('genero')
+
+        professor = Professor(
+            nome=nome,
+            celular=celular,
+            email=email,
+            senha=senha,
+            materia=', '.join(materia),
+            nivel_ensino=', '.join(nivel_ensino),
+            idade = idade,
+            genero=genero
+        )
+        professor.save()
+        return redirect('loginA')
+    return render(request, 'cadastroAluno.html')
+
+def loginA(request):
+    if request.method == 'POST':
+        email = request.POST.get('loginEmail')
+        senha = request.POST.get('loginSenha')
+        
+        try:
+            professor = Professor.objects.get(email=email)
+            if professor.senha == senha:
+                request.session['professor_id'] = professor.id
+                return redirect('perfilP')
+            messages.error(request, 'Senha incorreta')
+        except Professor.DoesNotExist:
+            messages.error(request, 'Email não encontrado')
+    return render(request, 'loginAluno.html')
+
+                  
+
 def home(request):
     return render(request, 'landingPage.html')
 
-def cadastroP(request):
-    return render(request, 'cadastroProfessor.html')
 
 def busca(request):
     return render(request, 'busca.html')
