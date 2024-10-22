b892625a4a840cfc0a01e32a834a41fcbf0ae973
Vinícius Bernardo <vbs4@cesar.school>
2024-10-22 16:52:44 -0300

    criei a classe de avaliacao

--- which_teacher_app/models.py
+++ which_teacher_app/models.py
@@ -16,3 +16,11 @@ class Horario(models.Model):
     professor = models.ForeignKey(Professor, on_delete=models.CASCADE, related_name='horarios')
     dia = models.CharField(max_length=10)
     horario = models.TimeField()
+
+class Avaliacao(models.Model):
+    estrelas = models.IntegerField()
+    comentario = models.TextField(max_length=500)
+    id_aluno = models.IntegerField()
+    nome_aluno = models.CharField(max_length=100)
+    id_professor = models.IntegerField()
+    nome_professor = models.CharField()
