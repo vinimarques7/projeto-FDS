81413286fa8cc8e9df0b0c4d2b458a4d82d3885e
Vinícius Bernardo <vbs4@cesar.school>
2024-10-24 21:16:53 -0300

    fiz o teste formar turmas

--- cypress/e2e/formarTurmas.cy.js
+++ cypress/e2e/formarTurmas.cy.js
@@ -1,6 +1,14 @@
-describe('test suite Login', () => {
-    it('Formar turma com sucesso', () => {
-    cy.visit('/');
+Cypress.Commands.add('deleteAllUsersP', () => {
+    cy.exec('python delete_usersP.py', { failOnNonZeroExit: false })
+});
+
+Cypress.Commands.add('deleteAllUsersA', () => {
+    cy.exec('python delete_usersA.py', { failOnNonZeroExit: false })
+});
+
+
+Cypress.Commands.add('registerProfessor', () =>{
+    cy.deleteAllUsersP();
     cy.get('.btn-secondary').click()
     cy.get('.cadastro-section > .btn').click()
     cy.get('#nome').type('teste testador')
@@ -23,6 +31,41 @@ describe('test suite Login', () => {
     cy.get('#loginSenha').type('queijo')
     cy.get('form > .btn').click()
     
+})
+
+Cypress.Commands.add('registerAluno', () =>{
+    cy.deleteAllUsersA();
+    cy.get('.btn-primary').click()
+    cy.get('.cadastro-section > .btn').click()
+    cy.get('#nome').type('Aluno Barros da Silva')
+    cy.get('#celular').type('347543875')
+    cy.get('#email').type('aluno@gmail.com')
+    cy.get('#senha').type('senha')
+    cy.get('#male').check();
+    cy.get('#customRange3')
+    .trigger('mousedown', { which: 1 })
+    .trigger('mousemove', { clientX: 30 })
+    .trigger('mouseup');
+    cy.get('.dropdown > .btn').click()
+    cy.get('input[name="nivel_ensino"][value="Ensino-Fundamental"]').check();
+    cy.get('.dropdown > .btn').click()
+    cy.get('.mt-4.text-center > .btn').click()
+    
+})
+
+
+describe('test suite Login', () => {
+    it('Formar turma com sucesso', () => {
+    cy.visit('/');
+    cy.registerAluno();
+    cy.visit('/');
+    cy.registerProfessor();
+    cy.get('[data-toggle="modal"]').click();
+    cy.get('.modal-body > .form-control').type('Logaritmo')
+    cy.get('.modal-body > :nth-child(3) > .btn').click()
+    cy.get('input[name="materia"][value="Matemática"]').check();
+    cy.get('.modal-body > :nth-child(3) > .btn').click()
+    cy.get(':nth-child(5) > .btn').click()
 
     })
 })
