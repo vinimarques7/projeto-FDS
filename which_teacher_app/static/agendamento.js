19cb1c995af4390e5ef34e15a20d18bc637b9c36
Matheus Verissimo <matheusmverissimo820@gmail.com>
2024-10-28 00:26:10 -0300

    Refactor agendamento.js and loginAluno.html templates

--- which_teacher_app/static/agendamento.js
+++ which_teacher_app/static/agendamento.js
@@ -1,3 +1,7 @@
+function removeDiacritics(str) {
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     const horariosDataElement = document.getElementById('horarios-data');
     const horarios = JSON.parse(horariosDataElement.getAttribute('data-horarios'));
@@ -12,29 +16,57 @@ document.addEventListener('DOMContentLoaded', function () {
     // Exibe os horários disponíveis ao selecionar uma data
     selectedDateInput.addEventListener('change', function () {
         const selectedDate = new Date(selectedDateInput.value);
-        const selectedDayOfWeek = selectedDate.toLocaleString('pt-BR', { weekday: 'long' }).toLowerCase();
-        console.log("Dia selecionado:", selectedDayOfWeek);  // Verifique se o dia da semana corresponde corretamente
+        let selectedDayOfWeek = selectedDate.toLocaleString('pt-BR', { weekday: 'long' }).toLowerCase();
+        selectedDayOfWeek = removeDiacritics(selectedDayOfWeek); // Remover acentuação
+        console.log("Dia da semana selecionado:", selectedDayOfWeek);
 
         document.getElementById('selected-date').innerText = selectedDateInput.value;
         document.getElementById('data-selecionada-oculto').value = selectedDateInput.value;
 
         // Filtra os horários com base no dia da semana selecionado
-        const horariosDisponiveis = horarios.filter(horario => horario.dia.toLowerCase() === selectedDayOfWeek);
+        const horariosDisponiveis = horarios.filter(horario => {
+            const diaCadastrado = removeDiacritics(horario.dia.toLowerCase());
+            console.log(`Comparando dia cadastrado: "${diaCadastrado}" com o selecionado: "${selectedDayOfWeek}"`);
+            return diaCadastrado === selectedDayOfWeek;
+        });
+
         availableTimesElement.innerHTML = '';
 
         if (horariosDisponiveis.length > 0) {
             horariosDisponiveis.forEach(horario => {
                 let inicio = new Date(`1970-01-01T${horario.inicio}:00`);
                 let fim = new Date(`1970-01-01T${horario.fim}:00`);
-                
 
-                while (inicio < fim) {
-                    const timeSlot = document.createElement('div');
-                    timeSlot.classList.add('time-slot', 'badge', 'badge-primary', 'mr-2', 'mb-2');
-                    timeSlot.setAttribute('data-time', inicio.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}));
-                    timeSlot.innerText = `${inicio.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}`;
-                    availableTimesElement.appendChild(timeSlot);
-                    inicio.setMinutes(inicio.getMinutes() + 60);  // Intervalo de 1 hora
+                // Verifica se o horário final é menor que o horário inicial (caso atravesse a meia-noite)
+                if (fim < inicio) {
+                    let meiaNoite = new Date('1970-01-01T23:59:59');
+                    while (inicio <= meiaNoite) {
+                        const timeSlot = document.createElement('div');
+                        timeSlot.classList.add('time-slot', 'badge', 'badge-primary', 'mr-2', 'mb-2');
+                        timeSlot.setAttribute('data-time', inicio.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
+                        timeSlot.innerText = `${inicio.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}`;
+                        availableTimesElement.appendChild(timeSlot);
+                        inicio.setMinutes(inicio.getMinutes() + 60);
+                    }
+
+                    inicio = new Date('1970-01-02T00:00:00');
+                    while (inicio < fim) {
+                        const timeSlot = document.createElement('div');
+                        timeSlot.classList.add('time-slot', 'badge', 'badge-primary', 'mr-2', 'mb-2');
+                        timeSlot.setAttribute('data-time', inicio.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
+                        timeSlot.innerText = `${inicio.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}`;
+                        availableTimesElement.appendChild(timeSlot);
+                        inicio.setMinutes(inicio.getMinutes() + 60);
+                    }
+                } else {
+                    while (inicio < fim) {
+                        const timeSlot = document.createElement('div');
+                        timeSlot.classList.add('time-slot', 'badge', 'badge-primary', 'mr-2', 'mb-2');
+                        timeSlot.setAttribute('data-time', inicio.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
+                        timeSlot.innerText = `${inicio.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}`;
+                        availableTimesElement.appendChild(timeSlot);
+                        inicio.setMinutes(inicio.getMinutes() + 60);
+                    }
                 }
             });
         } else {
