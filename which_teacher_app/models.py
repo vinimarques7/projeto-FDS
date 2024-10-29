f30cfb96791ad8862413f5e2206a260e427d0b95
Matheus Verissimo <matheusmverissimo820@gmail.com>
2024-10-28 23:38:53 -0300

    Refactor models.py to remove Horario model and update Agendamento model

--- which_teacher_app/models.py
+++ which_teacher_app/models.py
@@ -36,12 +36,12 @@ class Professor(models.Model):
     def __str__(self):
         return f"{self.nome} - {self.materia} ({self.nivel_ensino})"
 
-class Horario(models.Model):
-    professor = models.ForeignKey(Professor, on_delete=models.CASCADE, related_name='horarios')
-    dia_semana = models.CharField(max_length=100)
+# class Horario(models.Model):
+#     professor = models.ForeignKey(Professor, on_delete=models.CASCADE, related_name='horarios')
+#     dia_semana = models.CharField(max_length=100)
 
-    hora_inicio = models.TimeField(default=time(9, 0))  # Defina um valor padrão para hora de início
-    hora_fim = models.TimeField(default=time(17, 0))  
+#     hora_inicio = models.TimeField(default=time(9, 0))  Defina um valor padrão para hora de início
+#     hora_fim = models.TimeField(default=time(17, 0))  
 
 class Lembrete(models.Model):
     texto = models.CharField(max_length=255)
@@ -86,4 +86,4 @@ class Agendamento(models.Model):
     criado_em = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.aluno} - {self.professor} - {self.data} - {self.horario}"
+        return f"{self.aluno} - {self.professor} - {self.data}"
