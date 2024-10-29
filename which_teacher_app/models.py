6e80f41bf86d6b12de99979e1aa832db3e99e4fb
Fátima Beatriz Moraes <fbmmca@cesar.school>
2024-10-29 00:49:05 -0300

    adicao de forma de pagamento no cadastro

--- which_teacher_app/models.py
+++ which_teacher_app/models.py
@@ -2,13 +2,11 @@ from django.db import models
 from datetime import time
 from django.contrib.auth.models import User
 
-
 class Professor(models.Model):
     MATERIAS_CHOICES = [
         ('portugues', 'Português'),
         ('ingles', 'Inglês'),
         ('matematica', 'Matemática'),
-        # Adicione mais matérias conforme necessário
     ]
     
     NIVEIS_CHOICES = [
@@ -22,26 +20,24 @@ class Professor(models.Model):
     celular = models.CharField(max_length=15)
     email = models.EmailField(unique=True)
     senha = models.CharField(max_length=100)
-
     materia = models.CharField(max_length=255, blank=True, null=True)
     recebimento = models.TextField()
     comunicacao = models.TextField()
-    nivel_ensino = models.TextField()
+    nivel_ensino = models.CharField(max_length=100, choices=NIVEIS_CHOICES)
     genero = models.CharField(max_length=10)
     imagem = models.ImageField(upload_to='perfil_professor/', blank=True, null=True)
-    
-    
-    
 
     def __str__(self):
         return f"{self.nome} - {self.materia} ({self.nivel_ensino})"
 
-# class Horario(models.Model):
-#     professor = models.ForeignKey(Professor, on_delete=models.CASCADE, related_name='horarios')
-#     dia_semana = models.CharField(max_length=100)
+class Horario(models.Model):
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE, related_name='horarios')
+    dia_semana = models.CharField(max_length=20)
+    hora_inicio = models.TimeField(default=time(9, 0))  # Defina um valor padrão para hora de início
+    hora_fim = models.TimeField(default=time(17, 0))
 
-#     hora_inicio = models.TimeField(default=time(9, 0))  Defina um valor padrão para hora de início
-#     hora_fim = models.TimeField(default=time(17, 0))  
+    def __str__(self):
+        return f"{self.professor.nome} - {self.dia_semana}: {self.hora_inicio} às {self.hora_fim}"
 
 class Lembrete(models.Model):
     texto = models.CharField(max_length=255)
@@ -50,12 +46,14 @@ class Lembrete(models.Model):
     def __str__(self):
         return self.texto
 
-
 class Avaliacao(models.Model):
     estrelas = models.IntegerField()
     comentario = models.TextField(max_length=500)
     id_aluno = models.IntegerField()
     nome_aluno = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.nome_aluno} - {self.estrelas} estrelas"
 
 class Aluno(models.Model):
     id = models.BigAutoField(primary_key=True)
@@ -63,9 +61,12 @@ class Aluno(models.Model):
     celular = models.CharField(max_length=15)
     email = models.EmailField(unique=True)
     senha = models.CharField(max_length=100)
-    nivel_ensino = models.TextField()
+    nivel_ensino = models.CharField(max_length=100)
     genero = models.CharField(max_length=10)
-    idade = models.CharField(max_length=100)
+    idade = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.nome
 
 class Turma(models.Model):
     nome = models.CharField(max_length=100)
@@ -77,8 +78,8 @@ class Turma(models.Model):
         return self.nome
 
 class Agendamento(models.Model):
-    professor = models.ForeignKey("Professor", on_delete=models.CASCADE)
-    aluno = models.ForeignKey(User, on_delete=models.CASCADE)  # ou seu modelo de usuário
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    aluno = models.ForeignKey(User, on_delete=models.CASCADE)  # ou seu modelo de usuário personalizado
     data = models.DateField()
     meio_transmissao = models.CharField(max_length=50)
     meio_pagamento = models.CharField(max_length=50)
