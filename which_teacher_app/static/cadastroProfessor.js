49534fddd6898855275f1f170aabd8bf41d73583
Matheus Verissimo <matheusmverissimo820@gmail.com>
2024-11-10 01:44:10 -0300

    Refactor JavaScript and CSS files for improved readability and consistency

--- which_teacher_app/static/cadastroProfessor.js
+++ which_teacher_app/static/cadastroProfessor.js
@@ -1,9 +1,7 @@
-document.querySelectorAll('.dropdown-btn').forEach(btn => {
-    btn.addEventListener('click', event => {
-      event.preventDefault();
-      event.stopPropagation();
-      btn.parentNode.classList.toggle('show-dropdown');
-    });
-  });
-
-  
+document.querySelectorAll('.dropdown-btn').forEach((btn) => {
+	btn.addEventListener('click', (event) => {
+		event.preventDefault();
+		event.stopPropagation();
+		btn.parentNode.classList.toggle('show-dropdown');
+	});
+});
--- which_teacher_app/static/perfilpublicoP.js
+++ which_teacher_app/static/perfilpublicoP.js
@@ -2,21 +2,23 @@ let SubmitMenu = document.getElementById('menu-abrir');
 let menu = document.getElementById('menu-mobile');
 
 SubmitMenu.addEventListener('click', () => {
-    menu.classList.add('abrir-menu');
+	menu.classList.add('abrir-menu');
 });
 
 menu.addEventListener('click', () => {
-    menu.classList.remove('abrir-menu');
+	menu.classList.remove('abrir-menu');
 });
 
 // Modificando para Bootstrap 3
-document.querySelector('.btn-primary[data-toggle="modal"]').addEventListener('click', function () {
-    const nomeTurma = document.querySelector('.nome').value;
-    if (nomeTurma) {
-        const container = document.getElementById('turma-criada-container');
-        container.innerHTML = `<p>Turma criada: ${nomeTurma}</p>`;
-        
-        // Fechar o modal após criar a turma usando Bootstrap 3
-        $('#myModal').modal('hide');
-    }
-});
+document
+	.querySelector('.btn-primary[data-toggle="modal"]')
+	.addEventListener('click', function () {
+		const nomeTurma = document.querySelector('.nome').value;
+		if (nomeTurma) {
+			const container = document.getElementById('turma-criada-container');
+			container.innerHTML = `<p>Turma criada: ${nomeTurma}</p>`;
+
+			// Fechar o modal após criar a turma usando Bootstrap 3
+			$('#myModal').modal('hide');
+		}
+	});
